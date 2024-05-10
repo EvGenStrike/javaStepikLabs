7d31fdf421cc4ce6ab6303a003630f3b565033ac
EvGenStrike <petryakov_zhenya@mail.ru>
2024-05-10 17:43:57 +0500

    7 лаба

--- L7.1 Run/src/main/java/Main.java
+++ L7.1 Run/src/main/java/Main.java
@@ -1,55 +1,33 @@
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
-    private static final int THREADS_NUMBER = 10;
+    private static final int PORT = 5050;
+    private static final int MAX_THREADS = 10;
 
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < THREADS_NUMBER; ++i) {
-            Thread thread = new RandomSequenceExample();
-            //Thread thread = new SerialSequenceExample(i);
-            System.out.println("Start: " + thread.getName());
-            thread.start();
-        }
-    }
+    public static void main(String[] args) {
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
 
-    private static class RandomSequenceExample extends Thread {
-        public void run() {
-            //System.out.println("Run: " + this.getName());
-        }
-    }
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                System.out.println("Server started");
 
-    @SuppressWarnings("UnusedDeclaration")
-    private static class SerialSequenceExample extends Thread {
-        private static int currentMax = 1;
-        private int mainId;
-        private final static Object waitObject = new Object();
-
-        public SerialSequenceExample(int id) {
-            this.mainId = id;
-        }
-
-        public void run() {
-            try {
-                System.out.println("Run: " + mainId);
-                synchronized (waitObject) {
-                    while (mainId > currentMax) {
-                        waitObject.wait();
-                        //System.out.println("Awakened: " + mainId);
-                    }
-
-                    currentMax++;
-                    //System.out.println("Finished: " + mainId);
-                    waitObject.notifyAll();
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    executorService.submit(new ServerHandler(socket));
                 }
-            } catch (InterruptedException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                executorService.shutdown();
             }
+        }
+        catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
---
+++ L7.1 Run/src/main/java/ServerHandler.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ServerHandler implements Runnable {
+    private Socket socket;
+
+    public ServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+
+            String inputStr;
+            while ((inputStr = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(inputStr + "\n");
+                bufferedWriter.flush();
+                if (inputStr.equals("Bue.")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
